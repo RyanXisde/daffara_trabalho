879e73ce51c27b43d5c78bf4a53cec4eaeb10223
Ryan <ryan.strider@escola.pr.gov.br>
2026-07-01 11:14:03 -0300

    Update produtos.sql

--- produtos.sql
+++ produtos.sql
@@ -8,10 +8,21 @@ CREATE TABLE IF NOT EXISTS produtos(
 
 INSERT INTO produtos (nome, preco, venda, estoque)
 VALUES 
-('Camisa Geek', 85.00, 'unidade', 25 ),
-('Fernanda', '3435233', '000.000.000-01', 'sla nao entendii', '4321143' ),
-('Mario', '141324', '000.000.000-02', 'sla tambem', '34211423' ),
-('Joaquim', '1231231', '000.000.000-03', 'slasla', '213123'),
-('Italo Santos', '321312', '000.000-04', 'aaamnsla', '1221');
+('Camisa One Piece', 85.00, 'unidade', 25 ),
+('Conjunto Pijama Chainsaw-Man - Makim, Denj, Power', 139.99 , 'unidade', 14 ),
+('Mangá JJK Vol.1', 80.00 , 'unidade', 8 ),
+('Mangá JJK Vol.4', 75.99 , 'unidade', 13 ),
+('Mangá JJK Vol.11', 69.99 , 'unidade', 13 ),
+('Mangá Naruto Vol.2', 63.00 , 'unidade', 10 ),
+('Mangá Naruto Vol.11', 70.00 , 'unidade', 10 ),
+('Mangá Naruto Vol.3', 68.99 , 'unidade', 9 ),
+('Action Figure 1x', 100.00 , 'unidade', 7 ),
+('Caneca One Piece Going Mary', 99.99 , 'unidade', 9 ),
+('Novel Solo Leveling Vol.1', 60.00 , 'unidade', 7 ),
+('Novel Solo Leveling Vol.3', 60.00 , 'unidade', 8 ),
+('Novel Solo Leveling Vol.5', 55.00 , 'unidade', 11 ),
+('Mangá Classrom of Elite Vol.12', 60.00 , 'unidade', 16 ),
+('Mangá Classrom of Elite Vol.15', 47.00 , 'unidade', 16 ),
+('Mangá Naruto Vol.2', 63.00 , 'unidade', 10 ),
 
 SELECT * FROM produtos;
